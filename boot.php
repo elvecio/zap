6d7015ee5261bae9dbdda5110e9bec2d5eb7daa5
zotlabs <mike@macgirvin.com>
2016-11-23 16:08:33 -0800

    one server_role to rule them all

--- boot.php
+++ boot.php
@@ -618,15 +618,7 @@ function sys_boot() {
 
 	$a->convert();
 
-	if(defined('UNO')) {
-		if(UNO)
-			App::$config['system']['server_role'] = 'basic';
-		else
-			App::$config['system']['server_role'] = 'standard';
-	}
-
-	if(! (array_key_exists('server_role',App::$config['system']) && App::$config['system']['server_role']))
-		App::$config['system']['server_role'] = 'standard';
+	App::$config['system']['server_role'] = 'pro';
 
 	App::$timezone = ((App::$config['system']['timezone']) ? App::$config['system']['timezone'] : 'UTC');
 	date_default_timezone_set(App::$timezone);
